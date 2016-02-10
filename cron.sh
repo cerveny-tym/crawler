809e0fd85c4a82b3841718edcacdda26b6d2b072
Hardy Ferentschik <hardy@hibernate.org>
2016-02-10 14:39:52 +0100

    Adding processor execution

--- cron.sh
+++ cron.sh
@@ -2,3 +2,4 @@
 
 cd /f2f/crawler
 bundle exec ruby crawler.rb >> /f2f/crawler/crawl.log 2>&1
+java -DMONGODB_PASSWORD=redpassword  -jar word-processor-0.0.1-SNAPSHOT.jar  >> /f2f/crawler/processor.log 2>&1
